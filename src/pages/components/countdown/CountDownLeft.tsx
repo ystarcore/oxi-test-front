92965c512d32ad4da632ae9dd117f44643042ab0
fashion-cstar <ted228.dev@gmail.com>
2024-04-30 09:27:15 +0800

    update feedback

--- src/pages/components/countdown/CountDownLeft.tsx
+++ src/pages/components/countdown/CountDownLeft.tsx
@@ -3,7 +3,7 @@ export default function CountDownLeft() {
 
     return (
         <div className="w-[110px] sm:w-[130px] h-[110px] sm:h-[130px] bg-countdown-image flex items-center justify-center">
-            <div className="text-[#000] text-[30px] font-bold">500</div>
+            <div className="text-[#000] text-[30px] font-bold">210</div>
         </div>
     )
 }
--- src/pages/components/countdown/CountDownTitle.tsx
+++ src/pages/components/countdown/CountDownTitle.tsx
@@ -3,7 +3,7 @@ export default function CountDownTitle() {
 
     return (
         <div className="flex flex-col justify-center items-center">
-            <div className="text-[#F6C000] text-[28px] sm:text-[36px] uppercase font-bold">CountDown</div>
+            <div className="text-[#F6C000] text-[28px] sm:text-[36px] uppercase font-bold">Halving</div>
             <div className="text-[#AAA] text-[14px] sm:text-[18px] uppercase" style={{ letterSpacing: '0.3em' }}>Reward</div>
         </div>
     )
